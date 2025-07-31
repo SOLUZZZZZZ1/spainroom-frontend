b38189e132193f7efbac142988f8d60eefa262a0
SOLUZZZZZZ1 <soluzziona@gmail.com>
2025-07-31 14:33:31 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenido a SpainRoom</h1>
+      <p>Tu plataforma de alquiler inteligente.</p>
+    </div>
+  );
+}
+
+export default App;
