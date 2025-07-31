5dbbc3521cd3a679c5583a48345f117a2f8700d3
SOLUZZZZZZ1 <soluzziona@gmail.com>
2025-07-31 14:27:23 +0200

    Create RoomDetail.jsx

---
+++ src/RoomDetail.jsx/src/RoomDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RoomDetail() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Detalle de la habitación</h2>
+      <p>Aquí se mostrarán los datos completos de la habitación seleccionada: fotos, precio, ubicación, servicios, etc.</p>
+    </div>
+  );
+}
+
+export default RoomDetail;
