6ecb53e2b75b73eeb7b0ed0579251cbbadaa7e07
Ramón <ramon@spainroom.es>
2025-08-23 14:00:29 +0200

    fix: remove wrong navbar.css import from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import { NavLink, Link } from "react-router-dom";
-import "./navbar.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import SOSButton from "./components/SOSButton.jsx";
 
-export default function Navbar() {
+import Inicio from "./pages/Inicio.jsx";
+import Listado from "./pages/Listado.jsx";
+import Jobs from "./pages/Jobs.jsx";
+import Reservas from "./pages/Reservas.jsx";
+import Propietarios from "./pages/Propietarios.jsx";
+import Admin from "./pages/Admin.jsx";
+import Oportunidades from "./pages/Oportunidades.jsx";
+import HabitacionDetalle from "./pages/HabitacionDetalle.jsx";
+
+export default function App() {
   return (
-    <header className="sr-navbar">
-      <div className="sr-navbar__container">
-        <Link to="/" className="sr-navbar__brand" aria-label="SpainRoom - Inicio">
-          <img
-            src="/logo.png"
-            alt="SpainRoom"
-            className="sr-navbar__logo"
-            width="100"
-            height="auto"
-            onError={(e) => { e.currentTarget.style.display = "none"; }}
-          />
-        </Link>
-
-        <nav className="sr-navbar__nav" aria-label="Navegación principal">
-          <NavLink to="/" end className="sr-link">Inicio</NavLink>
-          <NavLink to="/listado" className="sr-link">Listado</NavLink>
-          <NavLink to="/jobs" className="sr-link">Jobs</NavLink>
-          <NavLink to="/reservas" className="sr-link">Reservas</NavLink>
-          <NavLink to="/propietarios" className="sr-link">Propietarios</NavLink>
-          <NavLink to="/admin" className="sr-link">Admin</NavLink>
-          <NavLink to="/oportunidades" className="sr-link">Oportunidades</NavLink>
-        </nav>
-      </div>
-    </header>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/listado" element={<Listado />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/reservas" element={<Reservas />} />
+        <Route path="/propietarios" element={<Propietarios />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/oportunidades" element={<Oportunidades />} />
+        <Route path="/habitaciones/:id" element={<HabitacionDetalle />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      <SOSButton />
+    </>
   );
 }
