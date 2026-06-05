dfe3a445609656ebd368001a7e8418fc60b0fb2b
mediazion <soluzziona@gmail.com>
2026-06-05 17:18:38 +0200

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,25 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
-export default function Navbar() {
-  const tabClass = ({ isActive }) => "sr-tab" + (isActive ? " active" : "");
+export default function Navbar(){
+  const tab = ({ isActive }) => "sr-tab" + (isActive ? " active" : "");
 
   return (
     <header className="sr-navbar">
       <div className="sr-row">
-        {/* Marca: logo pequeño blanco */}
         <Link to="/" aria-label="SpainRoom" className="sr-brand">
           <img src="/cabecera.png" alt="SpainRoom" className="sr-logo" />
           <span className="sr-brand-title">SpainRoom</span>
         </Link>
-
-        {/* Pestañas */}
         <nav className="sr-tabs" aria-label="Navegación principal">
-          <NavLink to="/" end className={tabClass}>Inicio</NavLink>
-          <NavLink to="/propietarios"   end className={tabClass}>Propietarios</NavLink>
-          <NavLink to="/inquilinos"     end className={tabClass}>Inquilinos</NavLink>
-          <NavLink to="/habitaciones"   end className={tabClass}>Habitaciones</NavLink>
-          <NavLink to="/oportunidades"  end className={tabClass}>Oportunidades</NavLink>
-          <NavLink to="/franquiciados"  end className={tabClass}>Franquiciados</NavLink>
-          <NavLink to="/reservas"       end className={tabClass}>Reservas</NavLink>
-          <NavLink to="/admin"          end className={tabClass}>Admin</NavLink>
+          <NavLink to="/" end className={tab}>🏠 INICIO 🏠</NavLink>
+          <NavLink to="/propietarios"  className={tab}>Propietarios</NavLink>
+          <NavLink to="/inquilinos"    className={tab}>Inquilinos</NavLink>
+          <NavLink to="/habitaciones"  className={tab}>Habitaciones</NavLink>
+          <NavLink to="/oportunidades" className={tab}>Oportunidades</NavLink>
+          <NavLink to="/franquiciados" className={tab}>Franquiciados</NavLink>
+          <NavLink to="/ayuda"         className={tab}>Ayuda</NavLink>
+          {/* Sin enlace público a /admin y sin “Mi Franquicia” aquí */}
         </nav>
       </div>
       <div className="sr-navbar-underline" />
